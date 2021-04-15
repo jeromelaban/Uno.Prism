be8a1537978697446f5bd920917e80f007826961
Jerome Laban <jerome.laban@nventive.com>
2021-04-15 15:13:49 -0400

    ci: Enable ui tests

--- build/scripts/wasm-uitest-run.sh
+++ build/scripts/wasm-uitest-run.sh
@@ -19,14 +19,14 @@ mono nuget.exe install NUnit.ConsoleRunner -Version 3.10.0
 export UNO_UITEST_TARGETURI=http://localhost:8000
 export UNO_UITEST_DRIVERPATH_CHROME=$BUILD_SOURCESDIRECTORY/build/node_modules/chromedriver/lib/chromedriver
 export UNO_UITEST_CHROME_BINARY_PATH=$BUILD_SOURCESDIRECTORY/build/node_modules/puppeteer/.local-chromium/linux-637110/chrome-linux/chrome
-export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/e2e/uno/wasm
+export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/e2e/uno/$TARGET_XAML_FLAVOR/wasm
 export UNO_UITEST_PLATFORM=Browser
 export UNO_UITEST_CHROME_CONTAINER_MODE=true
 
 mkdir -p $UNO_UITEST_SCREENSHOT_PATH
 
 # The python server serves the current working directory, and may be changed by the nunit runner
-bash -c "cd $BUILD_SOURCESDIRECTORY/e2e/Uno/HelloUnoWorld.Wasm/bin/Release/netstandard2.0/dist/; python server.py &"
+bash -c "cd $BUILD_SOURCESDIRECTORY/e2e/Uno/$TARGET_XAML_FLAVOR/HelloUnoWorld.Wasm/bin/Release/netstandard2.0/dist/; python server.py &"
 
 mono $BUILD_SOURCESDIRECTORY/build/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe \
 --trace=Verbose \
--- e2e/Uno/WinUI/HelloUnoWorld.WinUI.Skia.WPF/Program.cs
+++
@@ -1,3 +0,0 @@
-namespace HelloUnoWorld.WinUI.Skia.Gtk
-{
-}
