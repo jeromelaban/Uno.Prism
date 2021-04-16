c7d879aafb9f968923ded050765f086653312828
Jerome Laban <jerome.laban@nventive.com>
2021-04-16 08:29:09 -0400

    bump chromium

--- build/scripts/wasm-uitest-run.sh
+++ build/scripts/wasm-uitest-run.sh
@@ -10,8 +10,8 @@ dotnet build /p:Configuration=Release /p:DISABLE_GITVERSIONING=true $BUILD_SOURC
 
 cd $BUILD_SOURCESDIRECTORY/build
 
-npm i chromedriver@83.0.1
-npm i puppeteer@3.1.0
+npm i chromedriver@86.0.0
+npm i puppeteer@5.3.1
 
 # install dotnet serve
 dotnet tool install dotnet-serve --version 1.8.15 --tool-path $BUILD_SOURCESDIRECTORY/build/tools
@@ -22,7 +22,7 @@ mono nuget.exe install NUnit.ConsoleRunner -Version 3.10.0
 
 export UNO_UITEST_TARGETURI=http://localhost:8000
 export UNO_UITEST_DRIVERPATH_CHROME=$BUILD_SOURCESDIRECTORY/build/node_modules/chromedriver/lib/chromedriver
-export UNO_UITEST_CHROME_BINARY_PATH=$BUILD_SOURCESDIRECTORY/build/node_modules/puppeteer/.local-chromium/linux-756035/chrome-linux/chrome
+export UNO_UITEST_CHROME_BINARY_PATH=$BUILD_SOURCESDIRECTORY/build/node_modules/puppeteer/.local-chromium/linux-800071/chrome-linux/chrome
 export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/e2e/uno/$TARGET_XAML_FLAVOR/wasm
 export UNO_UITEST_PLATFORM=Browser
 export UNO_UITEST_CHROME_CONTAINER_MODE=true
